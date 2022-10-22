88aacc84971378dc1e8aa8d7bca77c9e434dee0b
Ujjwal Singh <86053991+s11ngh@users.noreply.github.com>
2022-10-22 11:29:24 -0400

    pushing the correct website

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from pydoc import render_doc
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('dropdown.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
